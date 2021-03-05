08e6fd0956b6553bb9566799a67a3ff2d1be6b3f
dimi2113V <vipvdi11@gmail.com>
2021-03-05 10:18:07 +0200

    I changed main file(it was add str and foo)

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-
+// I was chainging
     }
+        public static void initTest(){
+            String str = "Hello";
+            System.out.println(str);
+            int foo = 258-4785*1/2;
+            System.out.println(foo);
+        }
+
+
 }
